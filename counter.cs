f08308567c0ef54bd0a8d13ac1abe21eec65e18f
diego quiroga <db5026666@gmail.com>
2025-07-19 20:00:24 -0500

    [Diego]Add vowel counting functionality to Counter class

--- counter.cs
+++ counter.cs
@@ -2,6 +2,19 @@
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("por favor escriva una frase o palabra:");
+        string name = Console.ReadLine() ?? string.Empty;
 
+        int contadorVocales = 0;
+
+        foreach (char c in name)
+        {
+            if ("aeiou".Contains(c))
+            {
+                contadorVocales++;
+            }
+        }
+
+        Console.WriteLine("Numero de vocales: " + contadorVocales);
     }
 }
